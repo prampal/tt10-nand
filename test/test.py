b434e8ce4c8c1d121614e5326fdd24a90f00c9e3
prampal <93888290+prampal@users.noreply.github.com>
2025-12-05 22:55:53 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -25,16 +25,29 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+# Set the input values you want to test
+    dut.ui_in[0].value = 0
+    dut.ui_in[1].value = 0
+# Wait for a certain number of clock cycles as you choose to see the output values
+    await ClockCycles(dut.clk, 25)
+# The following assertion is just an example of how to check the output values.
+# Change it to match the actual expected output of your module:
+    assert dut.uo_out[0].value == 1
+    
+    dut.ui_in[0].value = 0
+    dut.ui_in[1].value = 1
+    await ClockCycles(dut.clk, 25)
+    assert dut.uo_out[0].value == 1
+    
+    dut.ui_in[0].value = 1
+    dut.ui_in[1].value = 0
+    await ClockCycles(dut.clk, 25)
+    assert dut.uo_out[0].value == 1
+    
+    dut.ui_in[0].value = 1
+    dut.ui_in[1].value = 1
+    await ClockCycles(dut.clk, 25)
+    assert dut.uo_out[0].value == 0
+    
+# Keep testing the module by changing the input values, waiting for
+# one or more clock cycles, and asserting the expected outputvalues.
